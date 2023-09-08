5550fb6d905d62aa1102945d1dff5bb769c28557
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-07 17:11:54 -0700

    Add '5-printf.c' showcasing the use of printf with precise grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Prints <with proper grammar, but the outcome is a piece of art,>
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
