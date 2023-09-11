ae3737ccea4471afe559300029367f1953d31f21
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-11 12:33:49 -0700

    Added README and updated 0x01-variables_if_else_while directory files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,6 @@
 int main(void)
 {
     int n;
-
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     /* Check if the number is positive, negative, or zero */
@@ -19,6 +18,5 @@ int main(void)
         printf("%d is negative\n", n);
     else
         printf("%d is zero\n", n);
-
     return (0);
 }
