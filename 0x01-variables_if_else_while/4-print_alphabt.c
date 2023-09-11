63317fc7796e7c4bcf8366b4a3294b5f42769420
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-11 14:09:50 -0700

    Implement alphabet printer excluding letters 'q' and 'e'

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
-/**
- * main - Prints the alphabet in lowercase, except q and e
- * Return: 0 if successful
- */
 int main(void)
 {
     char letter;
-
     for (letter = 'a'; letter <= 'z'; letter++)
     {
-        if (letter != 'q' && letter != 'e')
+        if (letter != 'e' && letter != 'q')
         {
             putchar(letter);
         }
     }
-
     putchar('\n');
-
     return (0);
 }
