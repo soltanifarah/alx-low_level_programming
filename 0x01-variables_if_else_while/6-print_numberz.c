e96ec34a78ceda6777048144f271f94888da74f9
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-11 15:24:56 -0700

    Implement single-digit base 10 number printing for task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Prints single-digit numbers of base 10
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int number;
+	int number;
 
-    for (number = '0'; number <= '9'; number++)
-    {
-        putchar(number);
-    }
-    putchar('\n');
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
