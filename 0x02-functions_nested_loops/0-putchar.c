22eef00b18a2190ed12be6b911b0da1457165766
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-13 11:40:49 -0700

    Implement 0-putchar.c: Program to print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,11 @@
-#include "main.h"
-
-/**
- * main - Prints "_putchar" followed by a newline.
- *
- * Return: Always 0 (success).
- */
-int main(void)
-{
-    char str[] = "_putchar\n";
-    int i;
-
-    for (i = 0; str[i]; i++)
-    {
-        _putchar(str[i]);
-    }
-
-    return (0);
+#include <stdio.h>  
+/**  
+ * main - Entry the point of the program  
+ *  
+ * Return: 0 for successful execution, non-zero for errors  
+ */  
+int main(void)  
+{  
+printf("_putchar\n");  
+return (0);  
 }
