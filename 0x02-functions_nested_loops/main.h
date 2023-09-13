01e471b34614a2d7694601b2cf322e4dc32cf5e3
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-13 11:43:58 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+/**  
+ * main - Entry the point of the program  
+ *  
+ * Return: 0 for successful execution, non-zero for errors  
+ */  
+int main(void)  
+{  
+printf("_putchar\n");  
+return (0);  
+}
