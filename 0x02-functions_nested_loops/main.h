079e98a66f80c67458cb1401eafd441c48cb2481
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-13 16:51:49 -0700

    H

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,11 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
-
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+int main(void);
 #endif /* MAIN_H */
