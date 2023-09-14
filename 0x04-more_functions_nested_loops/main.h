e8289b4102c8abda5a1d14dfc8f8ab284ec372b2
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-14 12:03:34 -0700

    Add _isupper function and update main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Include any necessary libraries here */
+
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
+/* 
+ * Prototypes of the functions you'll create.
+ * Make sure to add the prototype of each function you create to this file.
+ * For instance:
+ */
+ 
+int _isupper(int c);
+
+/* Add other function prototypes below */
+
+#endif /* MAIN_H */
