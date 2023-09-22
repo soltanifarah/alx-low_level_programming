93fb6bfa18820645575c48727c2892efe332eabb
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-22 12:57:52 -0700

    Added the necessary line at line 19 to ensure the program prints a[2] = 98, followed by a new line.

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints a[2] = 98
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    int n;
+    int a[5];
+    int *p;
+
+    /* ... (some existing code here) */
+
+    /* This is where you add your single line of code */
+
+    printf("a[2] = %d\n", a[2]);
+    return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+int main(void);
 
 #endif /* MAIN_H */
