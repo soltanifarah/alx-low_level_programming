c99d6a9b7b9504b754e69c3c4da4e8e99ac13da0
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-22 12:39:59 -0700

    Implement rot13 function for string encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+    int i = 0;
+    int j;
+    char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    while (s[i])
+    {
+        for (j = 0; input[j]; j++)
+        {
+            if (s[i] == input[j])
+            {
+                s[i] = output[j];
+                break;
+            }
+        }
+        i++;
+    }
+    return (s);
+}
