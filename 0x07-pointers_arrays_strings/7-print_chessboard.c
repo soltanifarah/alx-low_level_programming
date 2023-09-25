970eff3f061f8ed810f4bb2fec813652d5b3c09e
soltanifarah <soltanifarah.sl@gmail.com>
2023-09-25 14:39:34 -0700

    Implement function to visually render a chessboard in the terminal

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - Prints the chessboard.
- * @a: 2D array representing the chessboard.
- *
- * Return: void
+ * @a: The 2D array representing the chessboard.
  */
 void print_chessboard(char (*a)[8])
 {
     int i, j;
-
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
         {
-            putchar(a[i][j]);
+            _putchar(a[i][j]);
             if (j != 7)
-                putchar(' ');
+                _putchar(' ');
         }
-        putchar('\n');
+        _putchar('\n');
     }
 }
