b40ac25eb854ef1a3566b8a703b730ba25b1697d
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-04 15:01:43 -0700

    let's play with malloc/free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,40 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+
 /**
- * str_concat - str_concat
- * @s1: string
- * @s2: string
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: pointer.
+ * Return: pointer to a newly allocated space containing the concatenated string
+ * or NULL if the function fails
  */
 char *str_concat(char *s1, char *s2)
 {
-char *t;
-int i, size;
+    char *concat_str;
+    int len1 = 0, len2 = 0, i, j;
 
-if (s1 == NULL || s2 == NULL)
-{
-return (NULL);
-}
-size = strlen(s1);
+    /* If NULL is passed, treat as an empty string */
+    s1 = s1 ? s1 : "";
+    s2 = s2 ? s2 : "";
 
-t = (char *)malloc((sizeof(char) * (size + strlen(s1))) + 2);
+    /* Calculate length of s1 */
+    while (s1[len1])
+        len1++;
 
-if (t == NULL)
-{
-return (NULL);
-}
-else
-{
-for (i = 0; s1[i] != '\0'; i++)
-t[i] = s1[i];
+    /* Calculate length of s2 */
+    while (s2[len2])
+        len2++;
 
-for (i = 0; s2[i] != '\0'; i++)
-t[size + i] = s2[i];
-}
-t[size + i] = '\0';
+    /* Allocate memory for concatenated string */
+    concat_str = malloc((len1 + len2 + 1) * sizeof(char));
 
-return (t);
+    /* If memory allocation fails */
+    if (!concat_str)
+        return (NULL);
+
+    /* Copy s1 into concat_str */
+    for (i = 0; i < len1; i++)
+        concat_str[i] = s1[i];
+
+    /* Copy s2 into concat_str */
+    for (j = 0; j < len2; j++)
+        concat_str[i + j] = s2[j];
+
+    /* Null terminate the concatenated string */
+    concat_str[i + j] = '\0';
+
+    return (concat_str);
 }
