a842679d63a6dea149f9eeb5134b9f695f2f0648
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-04 14:52:58 -0700

    let's play with malloc/free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+    int i;
+
+    if (grid == NULL || height <= 0)
+    {
+        return;
+    }
+
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+
+    free(grid);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+0B. Malloc, Free - ALX Low Level Programming
+This directory contains exercises and tasks that introduce the concepts of dynamic memory allocation in C, using the functions malloc and free.
