e4730892ab69300e4bdd133aec6bb6bf7e027745
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-04 15:11:57 -0700

    let's play with malloc/free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,46 +2,39 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2D array of integers
- * @width: width of the grid
- * @height: height of the grid
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Return: pointer to the 2D array, or NULL on failure or if width/height <= 0
+ * Return: a pointer to a 2 dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **array, i, j;
+	int len = width * height;
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	if (len <= 0)
+		return (NULL);
 
-    grid = malloc(height * sizeof(int *));
-    if (grid == NULL)
-    {
-        return (NULL);
-    }
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(width * sizeof(int));
-        if (grid[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-            {
-                free(grid[j]);
-            }
-            free(grid);
-            return (NULL);
-        }
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
 
-        for (j = 0; j < width; j++)
-        {
-            grid[i][j] = 0;
-        }
-    }
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
 
-    return (grid);
+	return (array);
 }
