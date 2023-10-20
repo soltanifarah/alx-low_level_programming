8b210f841460d8a597e6d9e7a69a84bf61336ac6
soltanifarah <soltanifarah.sl@gmail.com>
2023-10-20 06:04:45 -0700

    0x12_singly_linked_lists

---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Function prototype for print_list */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
